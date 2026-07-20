103bd94886d772cd02e7b45e61588afe18a4589a
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-07-20 18:51:51 -0500

    implementing tee

--- tee.c
+++ tee.c
@@ -1,19 +1,30 @@
 #include <error.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#define BUFSIZE 100
+#define BUFSIZE 2000
 int main() {
   // read from stdin until end of file
 
   int fd;
-
+  ssize_t index = 0;
   char buf[BUFSIZE];
+  ssize_t sizeOrStatus;
+  while ((sizeOrStatus = read(STDIN_FILENO, buf, BUFSIZE))) {
 
-  ssize_t status = read(STDIN_FILENO, buf, BUFSIZE);
-
-  if (status == -1) {
-    // exit code 1
-    return 1;
+    printf("%s", buf);
+    fflush(stdout);
+    if (sizeOrStatus == -1) {
+      // exit code 1
+      printf("%s", "returning error code 1");
+      fflush(stdout);
+      return 1;
+    } else if (sizeOrStatus == 0) {
+      printf("%s", "printing at end of file");
+      printf("%s", buf);
+      fflush(stdout);
+      break;
+    }
   }
 }
